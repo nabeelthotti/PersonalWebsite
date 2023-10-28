bf236f706bf31ce7013dd3ba14e0c360f24fd9d8
Nabeel <nabeelthotti@eeindustries.org>
2023-10-28 16:42:27 -0700

    Updated site content

--- src/App.js
+++ src/App.js
@@ -2,34 +2,36 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [displayIntro, setDisplayIntro] = useState(true);
+  const [showText, setShowText] = useState(true);
 
   useEffect(() => {
-    // Set the timeout for the intro display
-    const timer = setTimeout(() => {
-      setDisplayIntro(false);
-    }, 4000); // Time is in ms (4000 ms = 4 seconds)
+    // When the component is mounted, wait for 4 seconds, then hide the text.
+    const timeoutId = setTimeout(() => {
+      setShowText(false);
+    }, 4000); // 4000ms = 4s
 
-    // Clear the timeout if the component is unmounted
-    return () => clearTimeout(timer);
+    // Cleanup function
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, []);
 
   return (
     <div className="App">
-      {displayIntro ? (
-        <header className="App-header">
+      <header className="App-header">
+        {showText ? (
           <h1 className="animated-text">Hello World...</h1>
-        </header>
-      ) : (
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Download CV</li>
-          </ul>
-        </nav>
-      )}
+        ) : (
+          <div className="Navigation">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Experience</li>
+              <li>Download CV</li>
+            </ul>
+          </div>
+        )}
+      </header>
     </div>
   );
 }
