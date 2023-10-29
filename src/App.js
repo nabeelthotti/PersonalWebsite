9bf3e8244f240b21ae7c7f7e9174ab785acabd48
Nabeel <nabeelthotti@eeindustries.org>
2023-10-28 19:14:51 -0700

    Updated site pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home'; // Import your components
 import About from './About';
 import Experience from './Experience';
 import DownloadCV from './DownloadCV';
@@ -11,38 +11,38 @@ function App() {
   const [showText, setShowText] = useState(true);
 
   useEffect(() => {
-    if (showText) {
-      setTimeout(() => {
-        setShowText(false);
-      }, 4000);  // Adjust time as needed
-    }
-  }, [showText]);
+    const timeoutId = setTimeout(() => {
+      setShowText(false);
+    }, 4000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  const renderNavigation = () => (
+    <div className="Navigation">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/experience">Experience</Link></li>
+        <li><Link to="/download-cv">Download CV</Link></li>
+      </ul>
+    </div>
+  );
 
   return (
     <Router>
       <div className="App">
-        {showText ? (
-          <header className="App-header">
-            <h1 className="animated-text">Hello World...</h1>
-          </header>
-        ) : (
-          <>
-            <nav>
-              <ul className="App-nav">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/experience">Experience</Link></li>
-                <li><Link to="/download-cv">Download CV</Link></li>
-              </ul>
-            </nav>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/experience" element={<Experience />} />
-              <Route path="/download-cv" element={<DownloadCV />} />
-            </Routes>
-          </>
-        )}
+        <header className="App-header">
+          {showText ? <h1 className="animated-text">Hello World...</h1> : renderNavigation()}
+        </header>
+
+        {/* Define your routes */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/download-cv" component={DownloadCV} />
+        </Switch>
       </div>
     </Router>
   );
--- src/Experience.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Experience = () => (
-  <div>
-    <h1>Experience Page</h1>
-  </div>
-);
-
-export default Experience;
