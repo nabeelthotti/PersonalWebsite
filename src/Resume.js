d6bf12c338b9deaed6630d8e94f3cec2de8c2051
Nabeel <nabeelthotti@eeindustries.org>
2023-11-28 22:59:49 -0800

    Added Resume

--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Resume = () => (
   <div>
-            <h1>RESUME</h1>
+    <h1>RESUME</h1>
+    {/* Display the converted image of your resume */}
+    <img src="NabeelsReume.jpg" alt="Resume" style={{ width: '100%', height: 'auto' }} />
+    
+    {/* Link to download the original PDF */}
+    <a href="NabeelsResume.pdf" download="Your_Resume_Name.pdf">
+      <button style={{ marginTop: '20px' }}>Download</button>
+    </a>
   </div>
 );
 
